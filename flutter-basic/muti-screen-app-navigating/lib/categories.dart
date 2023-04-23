de02436db490dee8aeade818cde734732c801d68
Shenghang Zhu <443880@student.fontys.nl>
2023-04-23 15:00:40 +0200

    model defined

---
+++ flutter-basic/muti-screen-app-navigating/lib/categories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Categories'),
+        ),
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            Text('1',style: TextStyle(color: Colors.white),),
+            Text('1',style: TextStyle(color: Colors.white),),
+            Text('1',style: TextStyle(color: Colors.white),),
+            Text('1',style: TextStyle(color: Colors.white),),
+          ],
+        ));
+  }
+}
