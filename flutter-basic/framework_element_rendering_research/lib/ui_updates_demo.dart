56e13ca510e74299ee21dfd23f071eb5d98d6959
Shenghang Zhu <443880@student.fontys.nl>
2023-04-23 14:40:48 +0200

    init

--- flutter-basic/framework_element_rendering_research/lib/ui_updates_demo.dart
+++ flutter-basic/framework_element_rendering_research/lib/ui_updates_demo.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 
-class UIUpdatesDemo extends StatefulWidget {
+import 'DemoButtons.dart';
+
+class UIUpdatesDemo extends StatelessWidget {
   const UIUpdatesDemo({super.key});
-
-  @override
-  StatefulElement createElement() {
-    print('UIUpdatesDemo CREATEELEMENT called');
-    return super.createElement();
-  }
-
-  @override
-  State<UIUpdatesDemo> createState() {
-    return _UIUpdatesDemo();
-  }
-}
-
-class _UIUpdatesDemo extends State<UIUpdatesDemo> {
-  var _isUnderstood = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,43 +11,22 @@ class _UIUpdatesDemo extends State<UIUpdatesDemo> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Center(
-        child: Column(
+        child:  Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
+          children: const [
+            Text(
               'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
               textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16),
-            const Text(
+            SizedBox(height: 16),
+            Text(
               'Do you understand how Flutter updates UIs?',
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = false;
-                    });
-                  },
-                  child: const Text('No'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = true;
-                    });
-                  },
-                  child: const Text('Yes'),
-                ),
-              ],
-            ),
-            if (_isUnderstood) const Text('Awesome!'),
+            SizedBox(height: 24),
+            DemoButtons(),
           ],
         ),
       ),
---
+++ flutter-basic/muti-screen-app-navigating/android/app/src/main/kotlin/com/example/muti_screen_app_navigating/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.muti_screen_app_navigating
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
