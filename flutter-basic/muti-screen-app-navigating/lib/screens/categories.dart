13d9c609a774e8bd59b77fba67904cdc641aaa88
Shenghang Zhu <443880@student.fontys.nl>
2023-04-25 18:29:31 +0200

    Explicit animation example

--- flutter-basic/muti-screen-app-navigating/lib/screens/categories.dart
+++ flutter-basic/muti-screen-app-navigating/lib/screens/categories.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:muti_screen_app_navigating/data/dummay_data.dart';
 import 'package:muti_screen_app_navigating/models/category.dart';
@@ -6,15 +6,39 @@ import 'package:muti_screen_app_navigating/widgets/category_grid_item.dart';
 import '../models/meal.dart';
 import 'meals.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {Key? key, required this.availableMeals})
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({Key? key, required this.availableMeals})
       : super(key: key);
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 300),
+        upperBound: 1,
+        lowerBound: 0);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -29,22 +53,38 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(25),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-              category: category,
-              onSelectedCategory: () {
-                _selectCategory(context, category);
-              })
-      ],
-    );
+    return AnimatedBuilder(
+        animation: _animationController,
+        child: GridView(
+          padding: const EdgeInsets.all(25),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(
+                  category: category,
+                  onSelectedCategory: () {
+                    _selectCategory(context, category);
+                  })
+          ],
+        ),
+        builder: (context, child) {
+          return SlideTransition(
+            position: Tween(
+              begin: const Offset(0, 0.3),
+              end: const Offset(0, 0),
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              ),
+            ),
+            child: child,
+          );
+        });
   }
 }
